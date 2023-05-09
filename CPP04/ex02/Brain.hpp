0cf2eb6183233777f100755d3156f68680b21794
TheJocker94 <danielecastellano94@gmail.com>
2023-05-09 14:41:34 +0200

    pino e porto ritornano

---
+++ CPP04/ex02/Brain.hpp
@@ -0,0 +1,16 @@
+#ifndef BRAIN_HPP
+#define BRAIN_HPP
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+class Brain
+{
+    public:
+        Brain();
+        ~Brain();
+        std::string ideas[100];
+};
+
+#endif
